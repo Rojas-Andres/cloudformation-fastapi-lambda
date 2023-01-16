987744b6fc97fd8cb7f7f83fe791153c261eb1e8
Rojas-Andres <andresfelipe200004@gmail.com>
2023-01-15 22:49:20 -0500

    implementacion fastapi cloudformation

--- modules/users/app.py
+++ modules/users/app.py
@@ -1,17 +1,30 @@
 from fastapi import FastAPI
+from starlette.requests import Request
+from mangum import Mangum
 
-# from mangum import Mangum
+from pydantic import BaseModel
+from typing import Optional
 
-# app = FastAPI()
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+    active: bool = True
+    admin: bool = False
 
+app = FastAPI()
 
-# @app.get("/users")
-# async def get_user():
-#     print("\n\n\nentre get users->")
-#     return {"message`": "Hello World!"}
-
-# lambda_handler = Mangum(app)
-
-def lambda_handler(event, context):
-    print("entre lambda handler")
+@app.get("/users")
+async def get_user(request: Request):
+    print("\n\n\nentre get users->")
+    print(request.scope["aws.event"])
     return {"message": "Hello World!"}
+
+@app.post("/users")
+async def create_user(request: Request, user: User):
+    print("\nPOST USER \n ")
+    print(request.scope["aws.event"])
+    print("user: ", user)
+    return {"message": "Hello desde el post!"}
+
+lambda_handler = Mangum(app)
